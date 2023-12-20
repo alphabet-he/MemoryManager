131359f7ec0c720e3fb687b786292869a84908c5
Skylar (remote) <alphabet_he@outlook.com>
2023-12-20 12:21:58 -0700

    delete original heap

--- FinalExam/OriginalHeap/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "HeapManagerUnitTest.h"
-
-int main() {
-    bool heapPassed = HeapManager_UnitTest();
-    if (heapPassed)
-        printf("heap passed\n");
-    else
-        printf("heap FAILED\n");
-}
